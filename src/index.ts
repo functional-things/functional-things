76e03ff547c5c54e1c60c83b45c9432e177b5564
Joseph R Miles <joe@jrm.dev>
2020-12-18 12:44:14 -0800

    fix: fixed bad postinstall command

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/**
+ * @file index.ts
+ * @author Joseph R Miles
+ * @summary Re-exports all sibling modules to create a catch-all import.
+ */
+
 export * from "./compose";
 export * from "./optics";
 export * from "./maybe";
