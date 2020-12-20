2304f619dcf0fd319c41ee14d97bed7af90fcfed
Joseph R Miles <joe@jrm.dev>
2020-12-20 14:37:38 -0800

    test: added tests for composeRight

--- src/compose.spec.ts
+++ src/compose.spec.ts
@@ -1,4 +1,4 @@
-import { compose } from "./compose";
+import { compose, composeRight } from "./compose";
 
 describe("compose", () =>
 {
@@ -20,6 +20,30 @@ describe("compose", () =>
       const multiplyTwo = (x: number) => 2 * x;
 
       expect(compose(multiplyTwo, subtractOne)(3)).toBe(-5);
+    });
+  });
+});
+
+describe("composeRight", () =>
+{
+  describe("When passed one function", () =>
+  {
+    const identity = <T extends any>(x: T): T => x;
+
+    it("Yields the same function", () =>
+    {
+      expect(composeRight(identity)(1)).toBe(1);
+    });
+  });
+
+  describe("When passed more than one function", () =>
+  {
+    it("Composes the functions from right to left.", () =>
+    {
+      const subtractOne = (x: number) => 1 - x;
+      const multiplyTwo = (x: number) => 2 * x;
+
+      expect(composeRight(multiplyTwo, subtractOne)(3)).toBe(-4);
     });
   });
 });
