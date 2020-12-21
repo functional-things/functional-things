a7df407a4cbe2f8ff486c29d4650558dfcbd36a7
Joseph R Miles <joe@jrm.dev>
2020-12-21 05:55:20 -0800

    feat: made arrayToList more type-strict

--- src/list.ts
+++ src/list.ts
@@ -4,5 +4,5 @@ export type List<T> = [ T, List<T> ] | [];
 export const cons = <T extends any>(a: T, b: List<T> = []): List<T> =>
   [ a, b ];
 
-export const arrayToList = (array: any[]) => 
-  array.reduceRight((acc, cur) => cons(cur, acc), []);
+export const arrayToList = <T extends any>(array: T[]): List<T> => 
+  array.reduceRight((acc, cur) => cons(cur, acc), [] as List<T>);
